f62cb09f0dd62471b8831912f011c166bb3868b5
Navneet Sharma <navneet.sharma@optevus.com>
2023-11-06 18:11:45 +0530

    connect to mongo atlas

---
+++ src/index.js
@@ -0,0 +1,32 @@
+// require("dotenv").config({ path: "./.env"});
+import dotenv from "dotenv";
+import connectDB from "./db/index.js";
+
+dotenv.config(
+    { path: "./env" }
+);
+
+connectDB();
+
+/* 
+import express from "express";
+const app = express();
+
+(async () => {
+  try {
+    await mongoose.connect(`${process.env.MONGO_URI}/${DB_NAME}`);
+    app.on("error", (error) => {
+      console.error("ERROR: ", error);
+      throw error;
+    });
+
+    app.listen(process.env.PORT, () => {
+      console.log(`Listening on port ${process.env.PORT}`);
+    });
+      
+  } catch (error) {
+    console.error("ERROR: ", error);
+    throw error;
+  }
+})();
+*/
