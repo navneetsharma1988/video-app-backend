49cb7c3e847dee53f6052b6eaa5de56d49d31961
Navneet Sharma <navneet.sharma@optevus.com>
2023-11-07 14:30:38 +0530

    utils added

--- src/index.js
+++ src/index.js
@@ -1,12 +1,24 @@
 // require("dotenv").config({ path: "./.env"});
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import app from "./app.js";
 
-dotenv.config(
-    { path: "./env" }
-);
+dotenv.config({ path: "./env" });
 
-connectDB();
+connectDB()
+  .then(() => {
+    console.log("MONGO db connection established !!! ");
+    app.on("error", (error) => {
+      console.error("ERROR: ", error);
+      throw error;
+    });
+    app.listen(process.env.PORT || 8000, () => {
+      console.log(`Server is running at port : ${process.env.PORT}`);
+    });
+  })
+  .catch((error) => {
+    console.log("MONGO db connection failed !!! ", error);
+  });
 
 /* 
 import express from "express";
---
+++ src/utils/asyncHandler.js
@@ -0,0 +1,19 @@
+const asyncHandler = (requestHandler) => {
+  (req, res, next) => {
+    Promise.resolve(requestHandler(req, res, next)).catch((err) => next(err));
+  };
+};
+
+export { asyncHandler };
+
+// With try catch approach
+// const asyncHandler = (fn) => async (req, res, next) => {
+//   try {
+//     await fn(req, res, next);
+//   } catch (error) {
+//     res.status(err.code || 500).json({
+//       success: false,
+//       message: err.message || "Internal Server Error",
+//     });
+//   }
+// };
